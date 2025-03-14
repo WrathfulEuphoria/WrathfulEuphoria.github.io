c433779e6a7278efbadeb5697fedf8b61a0b9e3e
prokoplp <WrathfulEuphoria@gmail.com>
2025-03-14 10:39:16 +0100

    Delete assets directory

--- assets/js/5e95c892.edc09888.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocusaurs=self.webpackChunkdocusaurs||[]).push([[9647],{8122:(s,e,r)=>{r.r(e),r.d(e,{default:()=>l});r(6540);var u=r(4164),a=r(1082),c=r(204),d=r(2831),n=r(1410),t=r(4848);function l(s){return(0,t.jsx)(a.e3,{className:(0,u.A)(c.G.wrapper.docsPages),children:(0,t.jsx)(n.A,{children:(0,d.v)(s.route.routes)})})}}}]);
--- assets/js/aba21aa0.e987507c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocusaurs=self.webpackChunkdocusaurs||[]).push([[5742],{7093:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
