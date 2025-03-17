b723afe79a5c782905543564f41246e634a6ec7b
proko <WrathfulEuphoria@gmail.com>
2025-03-17 14:01:59 +0100

    clean up

--- build/assets/js/a7456010.29cf55c1.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwompstuff=self.webpackChunkwompstuff||[]).push([[1235],{8552:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
--- build/assets/js/acecf23e.8ae9ba88.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwompstuff=self.webpackChunkwompstuff||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
